4c5603c808007879aca5e7df5719214474ba39e1
lava m <lavanymkanna@gmail.com>
2016-09-21 11:04:04 -0400

    core created

--- bookworm/settings.py
+++ bookworm/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'core',
     'bookcases',
     'books',
+
 )
 
 MIDDLEWARE_CLASSES = (
