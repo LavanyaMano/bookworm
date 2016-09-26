5e83160eaecbbdb16ca63be822e3a19c1d267f8c
lava m <lavanymkanna@gmail.com>
2016-09-26 13:39:18 -0400

    navbar responsive

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from .forms import LoginForm
+from . import views
 
 urlpatterns = [
+    url(r'^register/$',views.register, name = 'register'),
     url(r'^login/$','django.contrib.auth.views.login',{'authentication_form':LoginForm}, name='login'),
     url(r'^logout/$','django.contrib.auth.views.logout',{'template_name': 'accounts/logout.html'}, name='logout'),
 ]
