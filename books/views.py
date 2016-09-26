a8c8cff8e3e41ff589532f0a9b4f70aee916aa4c
lava m <lavanymkanna@gmail.com>
2016-09-26 10:36:33 -0400

    login page created

--- books/views.py
+++ books/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-from .models import Book
+from django.db.models import Q
+from .models import Book,Author
+
+def book_list(request):
+    books = Book.objects.all()
+    breadcrumbs = (("Books",),)
+    query = request.GET.get('q')
+    if query:
+        authors = Author.objects.filter(name__icontains = query)
+        books = books.filter(Q(title__icontains=query)|Q(authors__in = authors))
+        books = books.distinct()
+
+    context = {
+    "books": books,
+    "breadcrumbs":breadcrumbs
+    }
+    return render(request,"books/book_list.html",context)
+
+
 
 def book_detail(request,id):
     book = get_object_or_404(Book,pk=id)
--- bookworm/settings.py
+++ bookworm/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.core.urlresolvers import reverse_lazy 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django_extensions',
     'core',
+    'accounts',
     'bookcases',
     'books',
 
@@ -102,7 +104,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+LOGIN_REDIRECT_URL = reverse_lazy('books:book_list')
+LOGIN_URL = reverse_lazy('accounts:login')
+LOGOUT_URL = reverse_lazy('accounts:logout')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
