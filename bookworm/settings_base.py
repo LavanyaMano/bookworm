fd31e13a36d72c7b6567bdaa33be09a074d936a0
lava m <lavanymkanna@gmail.com>
2016-10-03 10:03:15 -0400

    pushing to Heroku

--- bookworm/settings_base.py
+++ bookworm/settings_base.py
@@ -24,9 +24,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+import dj_database_url
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+
 
 
 # Application definition
@@ -111,3 +119,4 @@ LOGOUT_URL = reverse_lazy('accounts:logout')
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
---
+++ bookworm/settings_local.py
@@ -0,0 +1,13 @@
+from .settings_base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+if DEBUG:
+    INSTALLED_APPS += ('debug_toolbar',)
